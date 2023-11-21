0229ac5c5b46dd3ad07378c92da068a770730655
jorgebarcelos <jorgebarcelos.faria@gmail.com>
2023-11-21 12:38:09 -0300

    :feat: Go webform html changes

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"web-form/handlers"
@@ -10,7 +9,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", handlers.SubscriptionHandlers)
-	fmt.Println("Servindo na porta: localhost:8080")
+	// fmt.Println("Servindo na porta: localhost:8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
